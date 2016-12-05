54d9e53b7bb11a70b2731e8f0c865f49c4828570
JessicaPancer <jessica.pancer@gmail.com>
2016-12-05 03:17:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -154,5 +154,38 @@ function fade(opacity) {
     .style("opacity", opacity);
   };
 }
+  
+  var access_token = "1581437709.f0b4b7e.fa83bf290d054d0b841fb0da2cffd6e9",
+    access_parameters = {
+        access_token: access_token
+    };
+
+function grabImages(tag, count, access_parameters) {
+    var instagramUrl = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&count=' + count;
+    $.getJSON(instagramUrl, access_parameters, onDataLoaded);
+}
+
+function onDataLoaded(instagram_data) {
+    var target = $("#target");
+    console.log(instagram_data);
+    if (instagram_data.meta.code == 200) {
+        var photos = instagram_data.data;
+        //console.log(photos);
+        if (photos.length > 0) {
+            target.empty();
+            for (var key in photos) {
+                var photo = photos[key];
+                target.append('<a href="' + photo.link + '"><img src="' + photo.images.thumbnail.url + '"></a>')
+            }
+        } else {
+            target.html("nothing found");
+        }
+    } else {
+        var error = instagram_data.meta.error_message;
+        target.html(error);
+    }
+}
+
+grabImages('unicorn', 40, access_parameters);
 </script>
 </html>
