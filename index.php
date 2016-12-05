6afddd80779e80b314a61e637cccc355321e87fc
JessicaPancer <jessica.pancer@gmail.com>
2016-12-05 03:42:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -161,7 +161,7 @@ function fade(opacity) {
     };
 
 function grabImages(tag, count, access_parameters) {
-    var instagramUrl = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?;
+    var instagramUrl = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?';
     $.getJSON(instagramUrl, access_parameters, onDataLoaded);
 }
 
