9df5ffb2d8af77d6cc3b607594815ca30cda118d
JessicaPancer <jessica.pancer@gmail.com>
2016-12-13 01:16:14 -0500

    Update photos.php

--- photos.php
+++ photos.php
@@ -6,8 +6,28 @@
   $place = $_GET["place"];
     echo '<p id="title"> Study abroad photos in '. $place .': </p>';
   ?>
-	<input action="action" type="button" value="Back" onclick="history.go(-1);" />
-
+<input action="action" type="button" value="Back" onclick="history.go(-1);" />
+<script type="text/javascript">
+	function grabImages() {
+	 var URL = 'https://api.flickr.com/services/rest/?method=flickr.places.find&api_key=b91bca9fe607c9115da17dafab2e6a08&query=Madrid&format=rest&format=json&jsoncallback=?'; 
+	 $.getJSON(URL, function(data){
+	  console.log(data);
+	    $.each(data.places.place, function(i, item){
+	      var URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b91bca9fe607c9115da17dafab2e6a08&tags=studyabroad&format=rest&format=json&jsoncallback=?&place_id=' + item.place_id;
+	     $.getJSON(URL, function(data){
+	      console.log(data);
+		$.each(data.photos.photo, function(i, item){
+		  // Creating the image URL. Info: http://www.flickr.com/services/api/misc.urls.html
+		  var img_src = "https://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_m.jpg";
+		  var img_thumb = $("<img/>").attr("src", img_src).css("margin", "8px")
+		  $(img_thumb).appendTo("#flickr-images");
+		});
+	      });
+	    });
+	  });
+	}
+	grabImages();
+</script>
 </center>
 
 </body>
