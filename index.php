5032c1a1c7347fea7e0b5d08d95f10d1d6b80cfd
JessicaPancer <jessica.pancer@gmail.com>
2016-12-13 01:29:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -155,33 +155,13 @@ function fade(opacity) {
     .style("opacity", opacity);
   };
 }
-  
-
-function grabImages() {
- var URL = 'https://api.flickr.com/services/rest/?method=flickr.places.find&api_key=b91bca9fe607c9115da17dafab2e6a08&query=Madrid&format=rest&format=json&jsoncallback=?'; 
- $.getJSON(URL, function(data){
-  console.log(data);
-    $.each(data.places.place, function(i, item){
-      var URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b91bca9fe607c9115da17dafab2e6a08&tags=studyabroad&format=rest&format=json&jsoncallback=?&place_id=' + item.place_id;
-     $.getJSON(URL, function(data){
-      console.log(data);
-        $.each(data.photos.photo, function(i, item){
-          // Creating the image URL. Info: http://www.flickr.com/services/api/misc.urls.html
-          var img_src = "https://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_m.jpg";
-          var img_thumb = $("<img/>").attr("src", img_src).css("margin", "8px")
-          $(img_thumb).appendTo("#flickr-images");
-        });
-      });
-    });
-  });
-}
-  
-grabImages();
 </script>
-<p id="Searchheader">View other people's study abroad photos:</p>
-<form action="photos.php">
-	<hr width="33%">
-	<input type="text" name="place" maxlength=100 placeholder="Search a location">
-	<input type="submit" value="Search">
-</form>
+<hr width="33%">
+<center>
+	<p id="Searchheader">View other people's study abroad photos:</p>
+	<form action="photos.php">
+		<input type="text" name="place" maxlength=100 placeholder="Search a location">
+		<input type="submit" value="Search">
+	</form>
+</center>
 </html>
