2130752e21813f5d5a4a7dd452b6d25ed705f0bb
JessicaPancer <jessica.pancer@gmail.com>
2016-12-13 01:42:55 -0500

    Update photos.php

--- photos.php
+++ photos.php
@@ -13,15 +13,12 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script type="text/javascript">
 	var place = "<?php echo $place;?>";
-	console.log(place);
 	function grabImages() {
-	 var URL = 'https://api.flickr.com/services/rest/?method=flickr.places.find&api_key=b91bca9fe607c9115da17dafab2e6a08&query=Madrid&format=rest&format=json&jsoncallback=?'; 
+	 var URL = 'https://api.flickr.com/services/rest/?method=flickr.places.find&api_key=b91bca9fe607c9115da17dafab2e6a08&query=' + place + '&format=rest&format=json&jsoncallback=?'; 
 	 $.getJSON(URL, function(data){
-	  console.log(data);
 	    $.each(data.places.place, function(i, item){
 	      var URL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b91bca9fe607c9115da17dafab2e6a08&tags=studyabroad&format=rest&format=json&jsoncallback=?&place_id=' + item.place_id;
 	     $.getJSON(URL, function(data){
-	      console.log(data);
 		$.each(data.photos.photo, function(i, item){
 		  // Creating the image URL. Info: http://www.flickr.com/services/api/misc.urls.html
 		  var img_src = "https://farm" + item.farm + ".static.flickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_m.jpg";
